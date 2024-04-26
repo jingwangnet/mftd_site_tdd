f777edb5db467b6a82a976050cb74e608a7096ae
jing Wang <hi@jingwang.me>
2024-04-26 21:34:58 +0800

    Add QuoteForm

---
+++ quotes/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Quote
+
+
+class QuoteForm(forms.ModelForm):
+    required_css_class = "required"
+
+    class Meta:
+        model = Quote
+        fields = [
+            "name",
+            "position",
+            "company",
+            "address",
+            "phone",
+            "email",
+            "web",
+            "description",
+            "sitestatus",
+            "priority",
+            "jobfile",
+            "quotedate" "quoteprice",
+        ]
