8d33badd25395e51739320f3ed622cdd08819bb0
jing Wang <hi@jingwang.me>
2024-04-28 14:38:53 +0800

    Show detail of quotes

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.quote_req, name="request"),
     path("show", views.QuoteList.as_view(), name="show"),
+    path("show/<int:pk>", views.QuoteDetail.as_view(), name="detail"),
 ]
 
 
